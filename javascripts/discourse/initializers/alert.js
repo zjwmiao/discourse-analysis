01fe1a29087043c03974c2db3704e396dedeab74
zjwmiao <1723168479@qq.com>
2025-09-22 14:10:39 +0800

    feature: mindspore

--- javascripts/discourse/initializers/alert.js
+++ javascripts/discourse/initializers/alert.js
@@ -17,13 +17,13 @@ export default {
       "https://unpkg.com/@opensig/open-analytics@0.0.9/dist/open-analytics.mjs"
     ).then(({ OpenAnalytics, getClientInfo, OpenEventKeys }) => {
       const oa = new OpenAnalytics({
-        appKey: "openEuler",
+        appKey: "MindSpore",
         request: (data) => {
           if (!isCookieAgreed()) {
             disableOA();
             return;
           }
-          fetch("/api-dsapi/query/track/openeuler", {
+          fetch("/api-dsapi/query/track/mindspore", {
             body: JSON.stringify(data),
             method: "POST",
             headers: { "Content-Type": "application/json" },
@@ -47,9 +47,9 @@ export default {
       const disableOA = () => {
         oa.enableReporting(false);
         [
-          "oa-openEuler-client",
-          "oa-openEuler-events",
-          "oa-openEuler-session",
+          "oa-MindSpore-client",
+          "oa-MindSpore-events",
+          "oa-MindSpore-session",
         ].forEach((key) => {
           localStorage.removeItem(key);
         });

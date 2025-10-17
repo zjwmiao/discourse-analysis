b9417998a48722d1fb9a4c56f241d2a472217c91
zjwmiao <1723168479@qq.com>
2025-10-17 10:00:20 +0800

    fix: lint问题修复、帖子点击、搜索结果点击上报数据问题修复

--- javascripts/discourse/lib/search.js
+++ javascripts/discourse/lib/search.js
@@ -1,4 +1,7 @@
+import $ from "jquery";
 import { debounce, onNodeInserted } from "./utils.js";
+
+let searchKey = "";
 
 function onClickSearchInput() {
   window._oaReport("click", {
@@ -39,33 +42,80 @@ function onClickSuggestion(ev) {
     type: "search-suggestion",
     module: "search",
     target: ev.currentTarget.textContent.trim(),
-    searchContent: window.$(".search-input-wrapper input").get(0).value,
+    searchContent: searchKey,
     detail: ev.currentTarget.href,
     $url: location.href,
   });
 }
 
 function onClickSearchResultTopic(ev) {
-  const current$ = window.$(ev.currentTarget);
+  const current$ = $(ev.currentTarget);
   window._oaReport("click", {
     type: "search-result",
     module: "search",
     target: current$.find(".first-line").text().trim(),
-    searchContent: window.$(".search-input-wrapper input").get(0).value,
-    detail: {
-      path: ev.currentTarget.href,
-      categories: current$.find(".badge-category__name").text().trim(),
-      tags: current$.find(".discourse-tags").text().trim(),
-    },
+    searchContent: searchKey,
+    path: ev.currentTarget.href,
+    categories: current$.find(".badge-category__name").text().trim(),
+    tags: current$.find(".discourse-tags").text().trim(),
+    $url: location.href,
+  });
+}
+
+function onAIClick(ev) {
+  if (ev.currentTarget.disabled) {
+    return;
+  }
+  window._oaReport("click", {
+    type: "ai-toggle",
+    module: "search",
+    searchContent: decodeURIComponent(location.search.match(/\bq=([^&]+)/)[1]),
+    detail: ev.currentTarget.getAttribute("aria-checked"),
+    $url: location.href,
+  });
+}
+
+// 搜索结果页帖子的点击
+function onClickSearchResultPageTopic(ev) {
+  let link = ev.target;
+  if (link === ev.currentTarget) {
+    return;
+  }
+  while (!link.classList.contains("search-link")) {
+    link = link.parentElement;
+    if (link === ev.currentTarget || !link) {
+      return;
+    }
+  }
+
+  let root = link;
+  while (!root.classList.contains("fps-result")) {
+    root = root.parentElement;
+    if (root === ev.currentTarget || !root) {
+      return;
+    }
+  }
+  const rank = [...root.parentElement.children].indexOf(root) + 1;
+
+  const target$ = $(root);
+  window._oaReport("click", {
+    type: "search-result",
+    module: "search",
+    target: $(link).text().trim(),
+    searchContent: decodeURIComponent(location.search.match(/\bq=([^&]+)/)[1]),
+    rank,
+    path: $(link).attr("href"),
+    categories: target$.find(".badge-category__name").text().trim(),
+    tags: target$.find(".discourse-tags").text().trim(),
     $url: location.href,
   });
 }
 
 export default function reportSearch() {
   onNodeInserted(".search-input-wrapper input", (node) => {
-    window.$(node).on("focus", onClickSearchInput);
-    window.$(node).on("input", debounce(onInputSearchInput, 300));
-    window.$(node).on("keydown", (ev) => {
+    $(node).on("focus", onClickSearchInput);
+    $(node).on("input", debounce(onInputSearchInput, 300));
+    $(node).on("keydown", (ev) => {
       if (ev.key === "Enter") {
         window._oaReport("input", {
           type: "search",
@@ -79,13 +129,11 @@ export default function reportSearch() {
 
   // 历史记录点击
   onNodeInserted(".search-menu-panel .search-menu-recent", (node) => {
-    window
-      .$(node)
+    $(node)
       .children(".search-menu-assistant-item")
       .on("click", onClickSearchHistory);
     // 清除历史记录
-    window
-      .$(node)
+    $(node)
       .find(".clear-recent-searches")
       .on("click", onClearSearchHistoryClick);
   });
@@ -94,15 +142,13 @@ export default function reportSearch() {
   onNodeInserted(
     ".search-menu-panel .results div[class^=search-result]",
     (node) => {
+      searchKey = $(".search-input-wrapper input").get(0).value;
       if (node.classList.contains("search-result-topic")) {
         // 话题结果点击
-        window
-          .$(node)
-          .find(".list .item a")
-          .on("click", onClickSearchResultTopic);
+        $(node).find(".list .item a").on("click", onClickSearchResultTopic);
       } else {
         // 联想结果点击
-        window.$(node).find(".list .item a").on("click", onClickSuggestion);
+        $(node).find(".list .item a").on("click", onClickSuggestion);
       }
     }
   );
@@ -110,56 +156,20 @@ export default function reportSearch() {
   window.addEventListener("afterRouteChange", ({ detail }) => {
     if (detail.to === "/search") {
       // 监听是否显示AI搜索结果的点击
-      onNodeInserted('.search-advanced .semantic-search__results-toggle', (el) => {
-        window.$(el).on("click", (ev) => {
-          if (ev.currentTarget.disabled) return;
-          window._oaReport("click", {
-            type: "ai-toggle",
-            module: "search",
-            searchContent: decodeURIComponent(
-              location.search.match(/\bq=([^&]+)/)[1]
-            ),
-            detail: ev.currentTarget.getAttribute("aria-checked"),
-            $url: location.href,
-          });
-        });
-      });
+      onNodeInserted(
+        ".search-advanced .semantic-search__results-toggle",
+        (el) => {
+          $(el).on("click", onAIClick);
+        }
+      );
 
       onNodeInserted(
         ".search-results .fps-result:first-child",
         () => {
-          window.$(".search-results .fps-result-entries").on("click", (ev) => {
-            let link = ev.target;
-            if (link === ev.currentTarget) return;
-            while (!link.classList.contains("search-link")) {
-              link = link.parentElement;
-              if (link === ev.currentTarget || !link) return;
-            }
-
-            let root = link;
-            while (!root.classList.contains("fps-result")) {
-              root = root.parentElement;
-              if (root === ev.currentTarget || !root) return;
-            }
-            const rank = [...root.parentElement.children].indexOf(root) + 1
-
-            const target$ = window.$(root);
-            window._oaReport("click", {
-              type: "search-result",
-              module: "search",
-              target: window.$(link).text().trim(),
-              searchContent: decodeURIComponent(
-                location.search.match(/\bq=([^&]+)/)[1]
-              ),
-              detail: {
-                rank,
-                path: window.$(link).attr("href"),
-                categories: target$.find(".badge-category__name").text().trim(),
-                tags: target$.find(".discourse-tags").text().trim(),
-              },
-              $url: location.href,
-            });
-          });
+          $(".search-results .fps-result-entries").on(
+            "click",
+            onClickSearchResultPageTopic
+          );
         },
         true
       );
--- javascripts/discourse/lib/topic.js
+++ javascripts/discourse/lib/topic.js
@@ -1,3 +1,4 @@
+import $ from "jquery";
 import { onNodeInserted } from "./utils.js";
 
 function onTopicListClick(ev) {
@@ -5,7 +6,9 @@ function onTopicListClick(ev) {
     return;
   }
   let target = ev.target;
-  while (!(target instanceof HTMLAnchorElement && target.classList.contains("title"))) {
+  while (
+    !(target instanceof HTMLAnchorElement && target.classList.contains("title"))
+  ) {
     target = target.parentElement;
     if (target === ev.currentTarget || !target) {
       return;
@@ -13,25 +16,28 @@ function onTopicListClick(ev) {
   }
 
   const mainLink = target.closest(".main-link");
-  const categories = [...mainLink.querySelectorAll('.badge-category__wrapper')].map(el => el.textContent.trim()).join();
-  const tags = mainLink.querySelector('.discourse-tags')?.textContent?.trim();
-  const path = target.getAttribute('href').match(/^\/t\/topic\/[^/]+/)?.[0];
-  sessionStorage.setItem('topicRead', JSON.stringify({
-    title: target.textContent.trim(),
-    path,
-    readTime: Date.now(),
-    categories,
-    ...(tags && { tags }),
-  }));
-  window._oaReport("click", {
-    target: target.textContent.trim(),
-    type: 'topic-click',
-    $url: location.href,
-    detail: {
+  const categories = [...mainLink.querySelectorAll(".badge-category__wrapper")]
+    .map((el) => el.textContent.trim())
+    .join();
+  const tags = mainLink.querySelector(".discourse-tags")?.textContent?.trim();
+  const path = target.getAttribute("href").match(/^\/t\/topic\/[^/]+/)?.[0];
+  sessionStorage.setItem(
+    "topicRead",
+    JSON.stringify({
+      title: target.textContent.trim(),
       path,
+      readTime: Date.now(),
       categories,
       ...(tags && { tags }),
-    }
+    })
+  );
+  window._oaReport("click", {
+    target: target.textContent.trim(),
+    type: "topic-click",
+    $url: location.href,
+    path,
+    categories,
+    ...(tags && { tags }),
   });
 }
 
@@ -39,16 +45,15 @@ function onTopicListClick(ev) {
  * 点击某个帖子
  */
 export function reportTopicClick() {
-  onNodeInserted(
-    '#main-outlet .topic-list .topic-list-body',
-    node => window.$(node).on('click', onTopicListClick)
+  onNodeInserted("#main-outlet .topic-list .topic-list-body", (node) =>
+    $(node).on("click", onTopicListClick)
   );
 }
 
 export function reportTopicLeave() {
-  window.addEventListener('afterRouteChange', ({ detail }) => {
-    if (detail.from.startsWith('/t/topic/')) {
-      const topicRead = sessionStorage.getItem('topicRead');
+  window.addEventListener("afterRouteChange", ({ detail }) => {
+    if (detail.from.startsWith("/t/topic/")) {
+      const topicRead = sessionStorage.getItem("topicRead");
       if (!topicRead) {
         return;
       }
@@ -56,19 +61,16 @@ export function reportTopicLeave() {
       if (readInfo.path !== detail.from.match(/^\/t\/topic\/[^/]+/)?.[0]) {
         return;
       }
-      sessionStorage.removeItem('topicRead');
-      window._oaReport(
-        'pageLeave',
-        {
-          target: readInfo.title,
-          detail: {
-            path: readInfo.path,
-            readTime: Date.now() - Number(readInfo.readTime),
-            categories: readInfo.categories,
-            ...(readInfo.tags ? { tags: readInfo.tags } : null),
-          }
-        }
-      );
+      sessionStorage.removeItem("topicRead");
+      window._oaReport("pageLeave", {
+        target: readInfo.title,
+        detail: {
+          path: readInfo.path,
+          readTime: Date.now() - Number(readInfo.readTime),
+          categories: readInfo.categories,
+          ...(readInfo.tags ? { tags: readInfo.tags } : null),
+        },
+      });
     }
   });
 }
